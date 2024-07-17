8d729149f0b441f8537fd9bde825e0f4debefc0e
OS-Samir <samiradhikary972@gmail.comr>
2024-07-17 23:09:32 +0545

    customreact

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
 
   return (
+    <>
     <h1>Hellooo Samir || React</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid est sint hic iusto voluptas qui, dolorem nesciunt nostrum mollitia atque odio sed numquam accusantium ipsum quia rem doloribus eaque porro.</p>
+    </>
   )
 }
 
