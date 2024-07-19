213e0cdccd19b315e4a0cb306e98fbe13c1282cd
OS-Samir <samiradhikary972@gmail.comr>
2024-07-19 23:19:18 +0545

    new files added

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,11 @@
 
 
 function App() {
-
+const username = "chai aur code"
 
   return (
     <>
-    <h1>Hellooo Samir || React</h1>
+    <h1>Hellooo Samir || React {username}</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid est sint hic iusto voluptas qui, dolorem nesciunt nostrum mollitia atque odio sed numquam accusantium ipsum quia rem doloribus eaque porro.</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { jsx as _jsx } from "react/jsx-runtime.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>Custome App</h1>
+    </div>
+  );
+}
+
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank',
+//   },
+//   children: 'CLick me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Click to visit google{" "}
+  </a>
+);
+
+const anotherUser = "chai aur react";
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit google",
+
+  anotherElement
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <App />
+  reactElement
+);
