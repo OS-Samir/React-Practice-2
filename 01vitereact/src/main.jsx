a5a25abdc4d0b2bada219907964627acb97270c7
OS-Samir <samiradhikary972@gmail.comr>
2024-07-19 23:41:26 +0545

    import removed

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { jsx as _jsx } from "react/jsx-runtime.js";
+// import { jsx as _jsx } from "react/jsx-runtime.js";
 
 function MyApp() {
   return (
