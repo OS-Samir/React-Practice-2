e769302dd2f26d7f4eac9f3f838ac3debaca2e72
OS-Samir <samiradhikary972@gmail.comr>
2024-07-20 13:03:12 +0545

    card

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,13 +1,21 @@
-
-import './App.css'
+import { Aggregate } from "mongoose";
+import "./App.css";
+import Card from "./Components/Card.jsx";
 
 function App() {
+  let myObj = {
+    username: "samir",
+    age: 21
+  }
 
-// const img = 'https://images.pexels.com/photos/21430948/pexels-photo-21430948/free-photo-of-a-man-holding-a-camera.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+  let newArr  = [3, 4, 5]
+  // const img = 'https://images.pexels.com/photos/21430948/pexels-photo-21430948/free-photo-of-a-man-holding-a-camera.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
   return (
     <>
-<h1 className='text-3xl animate-bounce bg-green-400 text-red-600 p-4 rounded-xl mb-4'>Hello </h1>
-{/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      <h1 className="text-3xl animate-bounce bg-green-400 text-red-600 p-4 rounded-xl mb-4">
+        Hello{" "}
+      </h1>
+      {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
   <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src={img} alt="" width="384" height="512"/>
   <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
@@ -27,28 +35,15 @@ function App() {
     </figcaption>
   </div>
 </figure> */}
-<div class="relative h-[400px] w-[300px] rounded-md ">
-  <img
-    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-    alt="AirMax Pro"
-    class="z-0 h-full w-full rounded-md object-cover"
-  />
-  <div class="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-  <div class="absolute bottom-4 left-4 text-left">
-    <h1 class="text-lg font-semibold text-white">Delba</h1>
-    <p class="mt-2 text-sm text-gray-300">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-      debitis?
-    </p>
-    <button class="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-      View Profile →
-    </button>
-  </div>
-</div>
+
+      <Card username= "samir" btnText = "clickme" />
+      <Card username="Saroj" /> 
+      
 
 
     </>
-  )
+  );
 }
 
-export default App
+export default App; 
+
