785d6ad066590d822f96c7575553451b0717290f
OS-Samir <samiradhikary972@gmail.comr>
2024-07-26 00:00:10 +0545

    new files added

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import About from './Components/AboutUs/About'
+import Home from './Components/Home/Home'
+import Header from './Components/Header/Header'
+
+
+
+const router = createBrowserRouter ([
+  {
+  path: '/',
+  element: <Layout />,
+  children: [
+    {
+      path: "",
+      element: <Home />,
+
+    },
+    {
+      path: "about",
+      element: <About />
+    }
+  ]
+}
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <RouterProvider router={router}  />
+   
   </React.StrictMode>,
 )
