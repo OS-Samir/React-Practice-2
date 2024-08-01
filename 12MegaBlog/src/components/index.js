18713260093a9edac6a01fe23304f46cfe97c297
OS-Samir <samiradhikary972@gmail.comr>
2024-08-01 17:37:46 +0545

    :

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,8 +1,12 @@
-import Header from "./Header/Header"
-
-import Footer from "./Footer/Footer"
-
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import container from "./container/container";
+import Logo from './Logo/Logo';
+import LogoutBtn from "./Header/LogoutBtn";
 export {
     Header, 
     Footer,
+    container,
+    Logo,
+    LogoutBtn,
 }
