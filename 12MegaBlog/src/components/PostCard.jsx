4da74cb2f6f0771b82f0869d5b36262790dd3cc6
OS-Samir <samiradhikary972@gmail.comr>
2024-08-02 23:26:01 +0545

    new lines added

--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -5,8 +5,8 @@ const PostCard = ({$id, title, featuredImage}) => {
   return (
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
-            <div className='w-full justify-center mb-4'>
-                
+            <div className='w-full justify-center m4'>
+
             </div>
         </div>
     </Link>
