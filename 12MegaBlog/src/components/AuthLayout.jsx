071e2eb071fadf1fd4e608b054f9b24b679f42f2
OS-Samir <samiradhikary972@gmail.comr>
2024-08-03 19:33:57 +0545

    changes made in AuthLayout file

---
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+ export default function Protected ({children, authentication = true }) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
