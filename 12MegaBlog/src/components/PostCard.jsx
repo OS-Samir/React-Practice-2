6b69570aeace064a74d2d8a8973404597e6a31ca
OS-Samir <samiradhikary972@gmail.comr>
2024-08-03 14:36:41 +0545

    changes in postcard file

--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -6,8 +6,12 @@ const PostCard = ({$id, title, featuredImage}) => {
     <Link to={`/post/${$id}`}>
         <div className='w-full bg-gray-100 rounded-xl p-4'>
             <div className='w-full justify-center m-4'>
-
+              <img src={appwriteService.getFilePreview(featuredImage)} alt={title} 
+              className='rounded-xl'/>
             </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
         </div>
     </Link>
   )
