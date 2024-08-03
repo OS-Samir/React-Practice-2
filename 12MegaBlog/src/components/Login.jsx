ad121a0320f131dec9879b8b64547ede6426b959
OS-Samir <samiradhikary972@gmail.comr>
2024-08-03 17:53:00 +0545

    login file created

---
+++ 12MegaBlog/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { login as authLogin } from '../store/authSlice'
+
+const Login = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
