fdc8b35ae22b3862277cf46aa642d928a61ea957
OS-Samir <samiradhikary972@gmail.comr>
2024-08-03 18:06:44 +0545

    import components

--- 12MegaBlog/src/components/Login.jsx
+++ 12MegaBlog/src/components/Login.jsx
@@ -1,11 +1,15 @@
 import React, {useState} from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { login as authLogin } from '../store/authSlice'
+import {Button, Input, Logo} from "./index"
+import {useDispatch} from "react-redux"
+import authService from '../appwrite/auth'
+import {useForm} from "react-hook-form"
 
 const Login = () => {
   return (
     <div>
-      
+      This is login page
     </div>
   )
 }
