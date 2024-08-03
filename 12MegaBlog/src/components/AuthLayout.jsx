527037a0daca0f9afdbac4df30d0432060d3aab7
OS-Samir <samiradhikary972@gmail.comr>
2024-08-03 19:35:31 +0545

    changes made in AuthLayout file

--- 12MegaBlog/src/components/AuthLayout.jsx
+++ 12MegaBlog/src/components/AuthLayout.jsx
@@ -3,6 +3,9 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
  export default function Protected ({children, authentication = true }) {
+
+    const navigate = useNavigate()
+    const [loader, setLoader] = useState(true)
   return (
     <div>
       
