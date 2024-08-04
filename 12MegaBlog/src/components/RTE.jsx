5a8ce85732775fd0616e6d358c7d1a84f1d2f712
OS-Samir <samiradhikary972@gmail.comr>
2024-08-04 11:45:38 +0545

    chnages in RTE file

--- 12MegaBlog/src/components/RTE.jsx
+++ 12MegaBlog/src/components/RTE.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import {Editor} from '@tinymce/tinymce-react';
+import { Controller } from 'react-hook-form';
 
 
-export const RTE = () => {
+export const RTE = ({name, control, label, defaultValue = ""}) => {
   return (
-    <Editor
-    initialValue='default value'
-    init={
-        {branding: false,
+   
+    <div className='w-full'> 
+    {label && <label className='inline-block mb-1 pl-1'>
+        {label}</label>}
+    <Controller 
+    name = {name || "content"}
+    control={control}
+    render={({field: {onChange}}) => (
+       <Editor 
+       initialValue= {defaultValue}
+       init={{
         height: 500,
-        menubar: true,
+        menubar: false,
         plugins: [
             'advlist autolink lists link image charmap print preview anchor',
             'searchreplace visualblocks code fullscreen',
             'insertdatetime media table paste code help wordcount'
 
         ],
-        }
-    } 
+        toolbar: 
+        'undo redo | formatselect | bold italic underline | \ alignleft aligncenter alignright alignjustify | \ bullist numlist outdent indent | removeformat | help'
+       }}
+       onEditorChange={onChange}
+       />
+    )}
     />
+
+
+     </div>
   )
 }
