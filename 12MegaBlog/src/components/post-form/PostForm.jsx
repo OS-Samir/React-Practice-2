92455b1509928e045209451be7f11757786fdddf
OS-Samir <samiradhikary972@gmail.comr>
2024-08-04 13:05:13 +0545

    changes in PostForm file

--- 12MegaBlog/src/components/post-form/PostForm.jsx
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -4,17 +4,19 @@ import {Button, Input, Select, RTE} from '../index'
 import appwriteService from "../../appwrite/config"
 import { useNavigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-const PostForm = () => {
+const PostForm = ({post}) => {
     const {register, handleSubmit, watch, setValue, control, getValues} = useForm({
             defaultValues: {
-                title: '',
-                content: '',
-                tags: [],
-                status: 'public',
-                cover: '',
+                title: post?.title || '',
+                slug: post?.slug || '',
+                content: post?.content || '',
+                status: post?.status || 'active',
             },
     })
+    const navigate = useNavigate()
+    const userData = useSelector(state => state.user.userData)
 
+    
   return (
     <div>
       
