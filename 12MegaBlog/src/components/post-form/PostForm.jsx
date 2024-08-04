feaa7ff26c9a17a6be9efa42f6af8eb1f1be25a8
OS-Samir <samiradhikary972@gmail.comr>
2024-08-04 13:21:44 +0545

    changes in PostForm file

--- 12MegaBlog/src/components/post-form/PostForm.jsx
+++ 12MegaBlog/src/components/post-form/PostForm.jsx
@@ -16,7 +16,24 @@ const PostForm = ({post}) => {
     const navigate = useNavigate()
     const userData = useSelector(state => state.user.userData)
 
-    
+    const submit = async (data) => {
+        if (post) {
+        const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null
+
+        if (file) {
+            appwriteService.deleteFile(post.featuredImage)
+        }
+        const dbPost = await appwriteService.updatePost(post.$id, {
+            ...data,
+            featuredImage: file ? file.$id : undefined
+
+            if (dbPost) {
+                
+            }
+        })  
+
+        }
+    }
   return (
     <div>
       
