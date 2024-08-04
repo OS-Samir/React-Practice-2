cd56f8024ad3a18611d6b6b951ecb82bfd7d0c14
OS-Samir <samiradhikary972@gmail.comr>
2024-08-04 12:45:44 +0545

    chnages in RTE file

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -3,10 +3,12 @@ import Footer from "./Footer/Footer";
 import container from "./container/container";
 import Logo from './Logo/Logo';
 import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
 export {
     Header, 
     Footer,
     container,
     Logo,
     LogoutBtn,
+    RTE
 }
