021c57cc2a963b0184df55e3bf743da1fe09a2d0
OS-Samir <samiradhikary972@gmail.comr>
2024-08-06 15:07:17 +0545

    app file modified

--- interview-discussion/src/App.jsx
+++ interview-discussion/src/App.jsx
@@ -1,22 +1,45 @@
-import React, {useState} from 'react'
+// import React, {useState} from 'react'
+
+// const App = () => {
+//     const [value, setValue] = useState(1)
+//     const [multipliedValue, setMultipliedValue] = useState(1)
+//     const multiplybyFive = () => {
+//         setMultipliedValue(value * 5)
+//         setValue( value + 1)
+//     }
+//   return (
+//     <>
+//     <h1> Main value: {value} </h1>
+//     <button
+//     onClick={multiplybyFive}>
+//         Click to multiply by 5
+//     </button>
+//     <h2> Multiplied value: {multipliedValue} </h2>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import React, {useState} from 'react';
+
+
 
 const App = () => {
+    console.log("App rendered");
     const [value, setValue] = useState(1)
-    const [multipliedValue, setMultipliedValue] = useState(1)
-    const multiplybyFive = () => {
-        setMultipliedValue(value * 5)
-        setValue( value + 1)
+
+    const clickMe = () => {
+        console.log("logged")
     }
   return (
-    <>
-    <h1> Main value: {value} </h1>
-    <button
-    onClick={multiplybyFive}>
-        Click to multiply by 5
-    </button>
-    <h2> Multiplied value: {multipliedValue} </h2>
-    </>
+    <div>
+      <h1>Main value: {value}</h1>
+      <button onClick={clickMe}>Click to multiply by 5</button>
+    </div>
   )
 }
 
 export default App
+
