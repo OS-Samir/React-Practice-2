6095b49d8d44fb6f1db2c3eca04ab85f8375cea2
OS-Samir <samiradhikary972@gmail.comr>
2024-08-07 17:57:50 +0545

    object created in server js file

--- FullStackBasic/Backend/server.js
+++ FullStackBasic/Backend/server.js
@@ -2,6 +2,35 @@ import express from 'express';
 
 const app = express();
 
-app.length('/', (req, res) => {
-    res.send('Server is ready');
-})  ;
+app.get('jokes', (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: 'First Joke'
+        },
+        {
+            id: 2,
+            title: 'A second joke'
+        },
+        {
+            id: 3,
+            title: 'This is a third joke'
+        },
+        {
+            id: 4,
+            title: 'This is a fourth joke'
+        },
+        {
+            id: 5,
+            title: 'This is a fifth joke'
+        }
+    ]
+});
+
+// get a list of 5 jokes
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+})
