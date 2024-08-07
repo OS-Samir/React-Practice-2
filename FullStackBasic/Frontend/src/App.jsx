14aecd210a62903b3f6e7f9d52c1aa4f320c741a
OS-Samir <samiradhikary972@gmail.comr>
2024-08-07 20:54:32 +0545

    app.jsx file updated

--- FullStackBasic/Frontend/src/App.jsx
+++ FullStackBasic/Frontend/src/App.jsx
@@ -1,7 +1,18 @@
-import React, {useState} from 'react'
-
+import React, {useEffect, useState} from 'react'
+import './App.css'
+import axios from 'axios'
 const App = () => {
   const [jokes, setJokes] = useState([])
+
+  useEffect(()=> {
+    axios.get('http://localhost:3000/jokes')
+    .then((response) => {
+      setJokes(response.data)
+    })
+    .catch ((error) => {
+      console.log(error)
+    })
+  })
   return (
     <div>
       <h1>Full stack</h1>
