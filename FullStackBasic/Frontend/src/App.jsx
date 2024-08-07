d1bee036e9f000ea32838dc0ab4e7ad26d78114f
OS-Samir <samiradhikary972@gmail.comr>
2024-08-07 21:32:25 +0545

    backend connected with frontend

--- FullStackBasic/Frontend/src/App.jsx
+++ FullStackBasic/Frontend/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [jokes, setJokes] = useState([])
 
   useEffect(()=> {
-    axios.get('http://localhost:3000/jokes')
+    axios.get('/api/jokes')
     .then((response) => {
       setJokes(response.data)
     })
@@ -18,12 +18,12 @@ const App = () => {
       <h1>Full stack</h1>
       <p>Jokes:{jokes.length}</p>
       {
-        jokes.map((joke, index)=> {
+        jokes.map((joke, index)=> (
             <div key={joke.id}>
               <h3>{joke.title}</h3>
               <p>{joke.content}</p>
             </div>
-        })
+        ))
       }
     </div>
   )
