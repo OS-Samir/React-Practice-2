45343cd58b6df14cce815e08d80688815a09841b
OS-Samir <samiradhikary972@gmail.comr>
2024-08-08 14:54:16 +0545

    app.jsx file changes

--- apihandling/frontend/src/App.jsx
+++ apihandling/frontend/src/App.jsx
@@ -1,12 +1,24 @@
 
 import './App.css'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect (() => {
+      ;(async () => {
+        const response = await axios.get('api/products')
+        console.log(response.data);
+        setProducts(response.data)
+      })()
+
+  },[]);
   return (
     <div>
       <h1>API handling</h1>
+      <h2>Number of products are: {products.length}</h2>
     </div>
   )
 }
