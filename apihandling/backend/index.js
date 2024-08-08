5ae3945d4140f1d7768c3ea2a454c1a6a006572e
OS-Samir <samiradhikary972@gmail.comr>
2024-08-08 13:36:58 +0545

    new files are added & modified

---
+++ apihandling/backend/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+const app = express();
+
+app.get('api/products', (req, res) => {
+    const products = [
+        {
+            id: 1, 
+            name: 'table wooden',
+            price: 300,
+            image: 'https://images.pexels.com/photos/25381426/pexels-photo-25381426/free-photo-of-lamp-above-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        },
+        {
+            id: 2,
+            name: 'table glass',
+            price: 200,
+            image: 'https://images.pexels.com/photos/25291886/pexels-photo-25291886/free-photo-of-coca-cola-with-ice.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        },
+        {
+            id: 3,
+            name: 'table plastic',
+            price: 400,
+            image: 'https://images.pexels.com/photos/1319572/pexels-photo-1319572.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+
+        },
+        {
+            id: 4,
+            name: 'table metal',
+            price: 350,
+            image: 'https://images.pexels.com/photos/4947396/pexels-photo-4947396.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        },
+        {
+            id: 5,
+            name: 'table polyester',
+            image: 'https://images.pexels.com/photos/25294262/pexels-photo-25294262/free-photo-of-view-of-glassware-and-flower-arrangements-on-the-table-in-a-palace.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
+        }
+    ]
+    setTimeout(() => {
+        res.send(products)
+    }, 3000)
+
+})
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=> {
+console.log(`server is running on port ${port}`);
+});
